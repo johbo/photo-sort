a62db06ee5bc9cd6ae4b65f7c9994efc6e9463e2
Johannes Bornhold <johannes@bornhold.name>
2015-01-16 21:16:41 +0100

    Change indentation to 4 spaces

--- image_sorter.d
+++ image_sorter.d
@@ -15,49 +15,49 @@ import deimos.freeimage;
 
 class ImageFileSorter {
 
-  private string _source_dir;
-  private string _target_dir;
+    private string _source_dir;
+    private string _target_dir;
 
-  this(string source_dir, string target_dir) {
-    _source_dir = source_dir;
-    _target_dir = target_dir;
-  }
-
-  void process_files(bool dry_run=false) {
-
-    writefln("Source dir: %s", _source_dir);
-    
-    // Images in current directory
-    auto files = dirEntries(_source_dir, SpanMode.shallow);
-    auto filtered_files = filter!should_process(files);
-
-    foreach(filename; filtered_files) {
-      writefln("Working on file %s", filename);
-
-      auto created = get_time(filename);
-      writefln("    created at: %s", created);
-
-      auto path = get_target_path(created);
-      writefln("    target path: %s", path);
-
-      auto target_path = buildPath(_target_dir, path);
-      if (! dry_run) {
-	ensure_path_exists(target_path);
-      }
-
-      auto target_filename = buildPath(target_path,
-				       get_target_filename(filename));
-      if (! target_filename.exists()) {
-	writefln("    moving file to %s", target_filename);
-	if (! dry_run) {
-	  rename(filename, target_filename);
-	}
-      } else {
-	writefln("    SKIPPING, file %s already exists!", target_filename);
-      }
+    this(string source_dir, string target_dir) {
+	_source_dir = source_dir;
+        _target_dir = target_dir;
     }
 
-  }
+    void process_files(bool dry_run=false) {
+
+        writefln("Source dir: %s", _source_dir);
+    
+        // Images in current directory
+        auto files = dirEntries(_source_dir, SpanMode.shallow);
+        auto filtered_files = filter!should_process(files);
+
+        foreach(filename; filtered_files) {
+            writefln("Working on file %s", filename);
+
+            auto created = get_time(filename);
+            writefln("    created at: %s", created);
+
+            auto path = get_target_path(created);
+            writefln("    target path: %s", path);
+
+            auto target_path = buildPath(_target_dir, path);
+            if (! dry_run) {
+                ensure_path_exists(target_path);
+            }
+
+            auto target_filename = buildPath(target_path,
+                                             get_target_filename(filename));
+            if (! target_filename.exists()) {
+                writefln("    moving file to %s", target_filename);
+                if (! dry_run) {
+                    rename(filename, target_filename);
+                }
+            } else {
+                writefln("    SKIPPING, file %s already exists!", target_filename);
+            }
+        }
+
+    }
 
 }
 
@@ -65,95 +65,96 @@ class ImageFileSorter {
 
 
 bool should_process(DirEntry item) {
-  auto supported = [".jpg", ".jpeg", ".cr2"];
-  return supported.canFind(item.name.toLower().extension());
+    auto supported = [".jpg", ".jpeg", ".cr2"];
+    return supported.canFind(item.name.toLower().extension());
 }
 
 
 SysTime get_time(DirEntry item) {
-  auto image_type = item.name.toLower().extension();
-  FREE_IMAGE_FORMAT[string] image_formats = [
-					     ".jpg": FIF_JPEG,
-					     ".jpeg": FIF_JPEG,
-					     ".cr2": FIF_RAW,
-					     ];
+    auto image_type = item.name.toLower().extension();
+    FREE_IMAGE_FORMAT[string] image_formats = [
+        ".jpg": FIF_JPEG,
+        ".jpeg": FIF_JPEG,
+        ".cr2": FIF_RAW,
+        ];
 
-  try {
-    return get_image_time(image_formats[image_type], item);
-  } catch (RangeError err) {
-    throw new Exception("Not supported image format");
-  }
+    try {
+        return get_image_time(image_formats[image_type], item);
+    } catch (RangeError err) {
+        throw new Exception("Not supported image format");
+    }
 }
 
 
 private SysTime get_image_time(FREE_IMAGE_FORMAT image_format, DirEntry item) {
 
-  auto image = FreeImage_Load(image_format,
-			      item.name.toStringz(),
-			      FIF_LOAD_NOPIXELS);
-  scope(exit) {
-    FreeImage_Unload(image);
-  }
-
-  FITAG* tag;
-  if (FreeImage_GetMetadata(FIMD_EXIF_MAIN,
-			    image,
-			    "DateTime".toStringz(),
-			    &tag)) {
-    auto tag_str = to!string(FreeImage_TagToString(image_format, tag));
-    return parse_ascii_date(tag_str);
-  } else {
-    // TODO: Dump tags should be kind of verbose mode only
-    auto iter = FreeImage_FindFirstMetadata(FIMD_EXIF_MAIN,
-					    image,
-					    &tag);
-    if (iter) {
-      writefln("TAG %s", FreeImage_GetTagKey(tag).fromStringz());
-      while (FreeImage_FindNextMetadata(iter, &tag)) {
-	writefln("TAG %s", FreeImage_GetTagKey(tag).fromStringz());
-      }
+    auto image = FreeImage_Load(image_format,
+                                item.name.toStringz(),
+                                FIF_LOAD_NOPIXELS);
+    scope(exit) {
+        FreeImage_Unload(image);
     }
-  }
 
-  // logging
-  writeln("INFO: Did not find an EXIF date, using file date.");
-  return item.timeLastModified();
+    FITAG* tag;
+    if (FreeImage_GetMetadata(FIMD_EXIF_MAIN,
+                              image,
+                              "DateTime".toStringz(),
+                              &tag)) {
+        auto tag_str = to!string(FreeImage_TagToString(image_format, tag));
+        return parse_ascii_date(tag_str);
+    } else {
+        // TODO: Dump tags should be kind of verbose mode only
+        auto iter = FreeImage_FindFirstMetadata(FIMD_EXIF_MAIN,
+                                                image,
+                                                &tag);
+        if (iter) {
+            writefln("TAG %s", FreeImage_GetTagKey(tag).fromStringz());
+            while (FreeImage_FindNextMetadata(iter, &tag)) {
+                writefln("TAG %s", FreeImage_GetTagKey(tag).fromStringz());
+            }
+        }
+    }
+
+    // logging
+    writeln("INFO: Did not find an EXIF date, using file date.");
+    return item.timeLastModified();
 }
 
 
 private SysTime parse_ascii_date(string ascii_date) {
-  ascii_date = ascii_date.replace(" ", ":");
+    ascii_date = ascii_date.replace(" ", ":");
 
-  auto parts = map!(to!int)(ascii_date.split(":"));
+    auto parts = map!(to!int)(ascii_date.split(":"));
 
-  if (parts.length == 6) {
-    return SysTime(DateTime(parts[0], parts[1], parts[2],
-			    parts[3], parts[4], parts[5]));
-  } else {
-    // TODO: logging
-    writeln("ERROR: Parsed date into %s parts. Original value %s.",
-	    parts.length, ascii_date);
-    throw new Exception("Cannot parse date");
-  }
+    if (parts.length == 6) {
+        return SysTime(DateTime(parts[0], parts[1], parts[2],
+                                parts[3], parts[4], parts[5]));
+    } else {
+        // TODO: logging
+        writeln("ERROR: Parsed date into %s parts. Original value %s.",
+                parts.length, ascii_date);
+        throw new Exception("Cannot parse date");
+    }
 }
 
 
 string get_target_path(SysTime time) {
-  return buildPath(time.year.to!string,
-		   format("%02d", time.month),
-		   format("%02d", time.day));
+    return buildPath(
+        time.year.to!string,
+        format("%02d", time.month),
+        format("%02d", time.day));
 }
 
 
 void ensure_path_exists(string path) {
-  if (! path.exists()) {
-    // TODO: logging
-    writefln("Creating path %s", path);
-    mkdirRecurse(path);
-  }
+    if (! path.exists()) {
+        // TODO: logging
+        writefln("Creating path %s", path);
+        mkdirRecurse(path);
+    }
 }
 
 
 string get_target_filename(string filename) {
-  return baseName(filename.toLower());
+    return baseName(filename.toLower());
 }
--- sort.d
+++ sort.d
@@ -13,52 +13,53 @@ import image_sorter;
 
 int main(string[] args) {
 
-  FreeImage_Initialise();
-  scope(exit) {
-    FreeImage_DeInitialise();
-  }
+    FreeImage_Initialise();
+    scope(exit) {
+        FreeImage_DeInitialise();
+    }
 
-  AppConfig config = AppConfig();
-  config.check_and_parse(args);
+    AppConfig config = AppConfig();
+    config.check_and_parse(args);
 
-  // TODO: How is logging done in D?
-  stdout.writefln("Working in directory %s", config.source_dir);
+    // TODO: How is logging done in D?
+    stdout.writefln("Working in directory %s", config.source_dir);
 
-  ImageFileSorter sorter = new ImageFileSorter(config.source_dir,
-					       config.target_dir);
-  sorter.process_files(config.dry_run);
-  
-  // Done, return success
-  return 0;
-  
+    ImageFileSorter sorter = new ImageFileSorter(
+        config.source_dir,
+        config.target_dir);
+    sorter.process_files(config.dry_run);
+
+    // Done, return success
+    return 0;
+
 }
 
 
 struct AppConfig {
 
-  string source_dir;
-  string target_dir;
+    string source_dir;
+    string target_dir;
 
-  bool dry_run = false;
+    bool dry_run = false;
 
-  void check_and_parse(string[] args) {
+    void check_and_parse(string[] args) {
 
-    // TODO: Want to say 'args.pop("--dry-run")' or similar
-    auto idx = args.countUntil("--dry-run");
-    if (idx != -1) {
-      dry_run = true;
-      args = args.remove(idx);
+        // TODO: Want to say 'args.pop("--dry-run")' or similar
+        auto idx = args.countUntil("--dry-run");
+        if (idx != -1) {
+            dry_run = true;
+            args = args.remove(idx);
+        }
+
+        // TODO: Probably there is a utility in D to parse command line
+        // arguments
+        if (args.length != 2) {
+            stderr.writefln("Usage: %s DIRNAME [--dry-run]", args[0]);
+            throw new Exception("Directory name missing");
+        }
+
+        source_dir = args[1];
+        target_dir = source_dir;
     }
-    
-    // TODO: Probably there is a utility in D to parse command line
-    // arguments
-    if (args.length != 2) {
-      stderr.writefln("Usage: %s DIRNAME [--dry-run]", args[0]);
-      throw new Exception("Directory name missing");
-    }
-
-    source_dir = args[1];
-    target_dir = source_dir;
-  }
 
 }
