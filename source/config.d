7041cf9dd2839a65ab7681d73157249c0680043c
Johannes Bornhold <johannes@bornhold.name>
2015-01-24 10:28:14 +0100

    Add tests to the config parser

--- source/config.d
+++ source/config.d
@@ -41,7 +41,42 @@ struct AppConfig {
         target_dir = source_dir;
 
         dry_run = arguments["--dry-run"].isTrue();
-
     }
+}
 
+
+unittest {
+
+    // TODO: Find out how to assign names to tests
+
+    // TODO: How to test that it tries to exit if I do not provide
+    // valid command line options?
+
+
+    // test: adjusts log level based on parameters
+
+    // TODO: avoiding to leave the globalLogLevel changed, better ways
+    // to achieve that?
+
+    auto oldLogLevel = globalLogLevel;
+    scope(exit) { globalLogLevel = oldLogLevel; }
+
+    auto config = new AppConfig();
+    auto args = ["program-name", "fake-directory"];
+    config.check_and_parse(args ~ ["--verbose"]);
+    assert(globalLogLevel == LogLevel.all);
+
+
+    // test: provides working directory as source and target
+    config.check_and_parse(args);
+    assert(config.source_dir == "fake-directory");
+    assert(config.target_dir == "fake-directory");
+
+
+    // test: allows to set the dry_run flag
+    config.check_and_parse(args);
+    assert(! config.dry_run);
+
+    config.check_and_parse(args ~ ["--dry-run"]);
+    assert(config.dry_run);
 }
