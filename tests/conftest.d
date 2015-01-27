dd9a6a925f1feb43e7da20523f9ba89436104763
Johannes Bornhold <johannes@bornhold.name>
2015-01-27 23:52:12 +0100

    Create a testing directory

--- tests/conftest.d
+++ tests/conftest.d
@@ -8,6 +8,14 @@ import image;
 auto testImagePath = "work/test-image.jpg";
 
 Image getTestImage() {
+
+    // TODO: use a utility to get a random directory here and clean it
+    // up afterwards
+    auto testingPath = "testing";
+    if (! testingPath.exists()) {
+        testingPath.mkdirRecurse();
+    }
+
     // TODO: provide a test image automatically
     assert(exists(testImagePath),
            "Make sure to have a test image available to run the tests.");
