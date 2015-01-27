e54ba48d1d2c8265464238c354e9d016228a5bd8
Johannes Bornhold <johannes@bornhold.name>
2015-01-28 00:02:02 +0100

    Generate a small test image
    Not yet very useful, still needs a bit of relevant EXIF data inside of it.

--- tests/conftest.d
+++ tests/conftest.d
@@ -1,6 +1,10 @@
 module tests.conftest;
 
 import std.file;
+import std.string;
+import std.path;
+
+import deimos.freeimage;
 
 import image;
 
@@ -12,9 +16,19 @@ Image getTestImage() {
     // TODO: use a utility to get a random directory here and clean it
     // up afterwards
     auto testingPath = "testing";
+    auto testingFile = buildPath("testing", "test-image.jpg");
+
     if (! testingPath.exists()) {
         testingPath.mkdirRecurse();
     }
+
+    auto testImage = FreeImage_Allocate(1, 1, 24);
+    scope(exit) {
+        FreeImage_Unload(testImage);
+    }
+    FreeImage_Save(FIF_JPEG, testImage, testingFile.toStringz());
+
+    // TODO: Add relevant EXIF information
 
     // TODO: provide a test image automatically
     assert(exists(testImagePath),
