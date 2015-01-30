4b24a3470f376e3ae21eed926b3fbfe5ce8385a4
Johannes Bornhold <johannes@bornhold.name>
2015-01-30 20:31:39 +0100

    Tried to create the exif data, but it seems not to be supported by freeimage
    Seems that writing exif data is not supported by freeimage. Will have to look for a different
    solution.

--- tests/conftest.d
+++ tests/conftest.d
@@ -1,10 +1,12 @@
 module tests.conftest;
 
+import std.conv;
 import std.file;
 import std.string;
 import std.path;
 
 import deimos.freeimage;
+import std.experimental.logger;
 
 import image;
 
@@ -26,9 +28,34 @@ Image getTestImage() {
     scope(exit) {
         FreeImage_Unload(testImage);
     }
-    FreeImage_Save(FIF_JPEG, testImage, testingFile.toStringz());
+
 
     // TODO: Add relevant EXIF information
+    FITAG* tag = FreeImage_CreateTag();
+    assert(tag != null, "Could not create tag");
+    scope(exit) {
+        FreeImage_DeleteTag(tag);
+    }
+
+    auto value = "2015:01:22 11:05:11";
+    uint length = to!uint(value.length + 1);
+    FreeImage_SetTagKey(tag, "DateTime".toStringz());
+    FreeImage_SetTagLength(tag, length);
+    FreeImage_SetTagCount(tag, length);
+    FreeImage_SetTagType(tag, FIDT_ASCII);
+    FreeImage_SetTagValue(tag, value.toStringz());
+
+    auto tag_success = FreeImage_SetMetadata(
+        FIMD_EXIF_MAIN,
+        testImage,
+        FreeImage_GetTagKey(tag),
+        tag);
+    if (!tag_success) {
+        throw new Exception("Writing the creation date failed");
+    }
+
+
+    FreeImage_Save(FIF_JPEG, testImage, testingFile.toStringz());
 
     // TODO: provide a test image automatically
     assert(exists(testImagePath),
